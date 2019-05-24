6541dfdc3e34a5cf8a6319995fb77f77be99ba62
Verta-master <vertyagina@gmail.com>
2019-05-24 11:33:19 +0600

    Update 2019-05-24T05:33:14.977Z

--- js/script.js
+++ js/script.js
@@ -8,6 +8,10 @@ $('.menu__btn').click(function(e) {
   $('.menu__list').slideToggle();
 });
   
+$('.top__menu-btn').click(function(e) {
+  $('.header .top__list').slideToggle();
+});
+  
 //Hide popups on escape and click outside
 $("body").click(function(){
   $('.menu__list').slideUp();
